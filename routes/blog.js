b91cd0ae1b567ad701520592d92646de7afae0fe
Ahgama sundar <ahgamasundar40@gmail.com>
2024-10-26 22:44:17 +0530

    completed create and view post

--- routes/blog.js
+++ routes/blog.js
@@ -33,7 +33,18 @@ router.get('/posts/:i', async function(req,res){
        return res.status(404).render('404');
         
     }
-    res.render('post-detail',{posts:posts[0]});
+    const postData={
+        ...posts[0],
+        date:posts[0].date.toISOString(),
+        humanReadableDate:posts[0].date.toLocaleDateString('en-US',{
+            weekday:"long",
+            year:"numeric",
+            month:"long",
+            day:"numeric"
+        })
+
+    }
+    res.render('post-detail',{posts:postData});
 })
 
 module.exports=router
